c6b9e222944b84619a0b76c486c5a5d88bad9b0d
Pedro Paulo Borges Mizael <pedro.mizael@perdomodoces.com.br>
2026-03-12 23:52:37 -0300

    Notebook com problema de dns 2

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,18 +1,13 @@
 import mongoose from "mongoose";
 
 async function connectToDatabase() {
+    /* mongoose.connect("mongodb+srv://admin:admin@cluster0.pdk6rx3.mongodb.net/livraria")
+        .then(() => console.log("Conectado no Mongo"))
+        .catch(err => console.log(err)); */
 
     await mongoose.connect(
-        "mongodb://admin:admin123@ac-vhz46ll-shard-00-00.pdk6rx3.mongodb.net:27017,ac-vhz46ll-shard-00-01.pdk6rx3.mongodb.net:27017,ac-vhz46ll-shard-00-02.pdk6rx3.mongodb.net:27017/livraria?ssl=true&replicaSet=atlas-6fc313-shard-0&authSource=admin"
+        "mongodb+srv://admin:admin123@cluster0.pdk6rx3.mongodb.net/?appName=Cluster0"
     );
-
-    mongoose.connection.on("error", err => {
-        console.log("Erro Mongo:", err);
-    });
-
-    mongoose.connection.once("open", () => {
-        console.log("Mongo conectado");
-    });
 
     return mongoose.connection;
 }
